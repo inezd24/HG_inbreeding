f2861f36648c5aa381ca3dedc2590c7dbe888109
Inez Derkx <61582196+inezd24@users.noreply.github.com>
2025-04-03 15:58:45 +0200

    Update Result2_Compare_relatedness.R

--- Result2_Compare_relatedness.R
+++ Result2_Compare_relatedness.R
@@ -1,4 +1,4 @@
-# Script developed by Inez Derkx, summer 2024
+# Script developed by Inez Derkx, summer 2024 and edited April 2025
 # Script aims to examine population relatedness in the Agta, Bayaka, and Raute
 
 ##################################### IMPORT LIBRARIES #####################################
@@ -10,105 +10,69 @@ library(dplyr)
 
 ##################################### IMPORT DATA #####################################
 
-spouses <- read.csv("/Users/inezd/PHD/Chapter_3/R_Files/spousal_information.csv", header = T, row.names = 1) # Df with info of all spouses
-raute_IBD_summary <- read.csv("/Users/inezd/PHD/Chapter_3/R_Files/raute_IBD_summary.csv", header = T, row.names = 1) # Df with IBD-based relatedness
-raute_7_kin <- read.delim("~/PHD/Genetic_Data/IBIS/raute_maf_7.coef",
-                          col.names = c("sample_1", "sample_2", "kinship_7", "IBD2_7", "segment_count", "degree_of_relatedness")) # Df with kinship coefficients
-id_gen # Df with different IDs of individuals
-raute_fam # Df with different IDs of individuals
-homozygosity # Df with FROH measures
+bayaka_permuted <- read.csv("/Users/inezd/Documents/Science/Raute/Chapter_3/R_Files/bayaka_permuted.csv", header = T, row.names = 1)
+agta_permuted <- read.csv("/Users/inezd/Documents/Science/Raute/Chapter_3/R_Files/agta_permuted.csv", header = T, row.names = 1)
+raute_permuted <- read.csv("/Users/inezd/Documents/Science/Raute/Chapter_3/R_Files/raute_permuted.csv", header = T, row.names = 1)
 
-##################################### TRANSFORM DATA #####################################
+agta_total <- read.csv("/Users/inezd/Documents/Science/Raute/Chapter_3/R_Files/agta_total.csv", header = T, row.names = 1)
+bayaka_total <- read.csv("/Users/inezd/Documents/Science/Raute/Chapter_3/R_Files/bayaka_total.csv", header = T, row.names = 1)
+raute_total <- read.csv("/Users/inezd/Documents/Science/Raute/Chapter_3/R_Files/raute_total.csv", header = T, row.names = 1)
 
-## AMONG TOTAL POPULATION
 
-# What does relatedness look like in the entire population
-population_relatedness <- raute_IBD_summary %>% 
-  left_join(raute_7_kin %>% select(kinship = kinship_7, pair), by = 'pair') %>%
-  left_join(spouses %>% select(pair, type), by = 'pair') %>%
-  mutate(type = replace_na(type, "non-spouse")) %>%
-  na.omit() %>%
-  mutate(id1 = sub("\\-.*", "", pair),
-         id2 = sub(".*-", "", pair)) %>%
-  left_join(id_gen %>% select(id1 = new_IID, gottra_born.1 = gottra_born, gottra_married.1 = gottra_married), by = 'id1') %>%
-  left_join(id_gen %>% select(id2 = new_IID, gottra_born.2 = gottra_born, gottra_married.2 = gottra_married), by = 'id2') %>%
-  mutate(gottra_marry.1 = if_else(is.na(gottra_married.1), gottra_born.1, gottra_married.1),
-         gottra_marry.2 = if_else(is.na(gottra_married.2), gottra_born.2, gottra_married.2),
-         gottra_combo = paste(pmin(gottra_marry.1, gottra_marry.2), 
-                              pmax(gottra_marry.1, gottra_marry.2), sep = "-"),
-         gottra_check = if_else(gottra_marry.1 == gottra_marry.2, "same", "different"))
+##################################### FIGURE 2A #####################################
 
-# Examine the probability of finding the observed values by permuting relatedness (see function)
-permuted_means <- permute_relatedness(population_relatedness)
+# Save the means
+observed_means <- data.frame(
+  population = c("Agta", "BaYaka", "Raute"),
+  observed_mean = c(0.02088135, 0.005302667, 0.124274), # this is with the Agta outlier
+  observed_mean2 <- c(0.01749632, 0.005302667, 0.124274)) # This is without the Agta outlier
 
-# Save in separate dataframe for later merging
-raute_allrel_per <- data.frame(dataset = 'total_population', kinship = permuted_means$permuted_means_df$sim)
-
-## AMONG ELIGIBLE INDIVIDUALS / POTENTIAL PARTNERS
-
-# Create new dataframe and save for later use
-raute_list <- raute_census_combined %>%
-  filter(alive2022 == "Yes") %>%
-  select(id_2014, sex, age2022, gottra_born, gottra_married) 
-write.csv(raute_list, "/Users/inezd/PHD/Genetic_Data/IBD_analyses/Analyses/raute_list.csv")
-
-# From this dataframe, generate pairs
-raute_pairs <- combn(raute_list$id_2014, 2) %>%
-  t() %>%
-  as.data.frame() %>%
-  rename(X1 = V1, X2 = V2) %>%
-  left_join(raute_list %>% 
-              select(X1 = id_2014, sex_x = sex, age_x = age2022), by = 'X1') %>%
-  left_join(raute_list %>% 
-              select(X2 = id_2014, sex_y = sex, age_y = age2022), by = 'X2') %>%
-  filter(sex_x != sex_y, age_x > 16, age_y > 16) %>% # Only individuals of opposite sex and older than 16 
-  left_join(id_gen %>% select(X1 = id_2014, ind.x= ind), by = 'X1') %>%
-  left_join(id_gen %>% select(X2 = id_2014, ind.y= ind), by = 'X2') %>%
-  left_join(raute_fam %>% select(ind.x = V1, id1 = new_IID), by = 'ind.x') %>%
-  left_join(raute_fam %>% select(ind.y = V1, id2 = new_IID), by = 'ind.y') %>%
-  select(c(9,10,3:6)) %>%
-  mutate(pair = paste(pmin(id1, id2), pmax(id1, id2), sep = '-')) %>%
-  left_join(population_relatedness, by = c('id1', 'id2', 'pair')) %>% # add relatedness values and gottra information
-  left_join(homozygosity %>% select(id1 = IID, FROH.x = FROH), by = 'id1') %>%
-  left_join(homozygosity %>% select(id2 = IID, FROH.y = FROH), by = 'id2') %>%
-  filter(kinship < 0.1768) #496 dyads
-write.csv(raute_pairs, "/Users/inezd/PHD/Genetic_Data/IBD_analyses/Analyses/raute_pairs.csv")
-
-# Visualize observed true mean versus distribution between spouses
-png("/Users/inezd/PHD/Chapter_3/Plots/eligible_kin_avg.png", height = 800, width = 800)
-eligible_kin <- ggplot(raute_pairs, aes(x = kinship)) +
-  geom_histogram(color = "#ebefff", fill="#839ffe", alpha = 0.6, bins = 50) +
-  geom_density(alpha=.2, fill="#FF6666") +
-  geom_segment(aes(x = 0.12, xend = 0.12, y = 0, yend = Inf), color = "#b77fff", linetype = 'dashed', linewidth = 1) + # amongst spouses // purple
+# Total population distribution
+total_datasets <- rbind(agta_total, bayaka_total, raute_total)
+png("/Users/inezd/Documents/Science/Raute/Chapter_3/Plots/pops_compare_total.png", height = 1000, width = 1000)
+total_rel_compare <- ggplot(rel_datasets_total, aes(x = kinship, fill = population)) +
+  geom_density(color = "#ebefff", aes(fill=population), alpha = 0.6) +
+  geom_vline(data = observed_means, aes(xintercept = observed_mean2, color = population), 
+             linetype = "dashed", size = 3) +
+  scale_fill_manual(values = c("Agta" = "#ffa635", 
+                               "BaYaka" = "#839ffe",
+                               "Raute" = "#ff839f")) +
+  scale_color_manual(values = c("Agta" = "#ffa635", 
+                               "BaYaka" = "#839ffe",
+                               "Raute" = "#ff839f")) +
   theme_classic() +
-  theme(text = element_text(size = 30)) +
-  scale_x_continuous(limits = c(0.05, 0.30), breaks = seq(0.05, 0.30, by = 0.05)) +
-  labs(title = "", x = 'Kinship coefficient', y = 'Frequency')
-spousal_kin
+  labs(title = "", x = 'Kinship coefficient', y = 'Frequency') +
+  theme(text = element_text(size = 40),
+        legend.position = 'none')
+total_rel_compare
 dev.off()
 
-# Examine the probability of finding the observed values by permuting relatedness (see function)
-permuted_means <- permute_relatedness(raute_pairs)
 
-# Save in separate dataframe
-raute_elirel_per <- data.frame(dataset = 'eligible_pairs', kinship = permuted_means$permuted_means_df$sim)
+##################################### FIGURE 2B #####################################
 
-# Plot
-png("/Users/inezd/PHD/Chapter_3/Plots/permuted_possibility.png", width = 800, height = 800)
-ggplot(permuted_means$permuted_means_df, aes(x=sim)) +
-  geom_histogram(color = "#ebefff", fill="#839ffe", alpha = 0.6, bins = 50) +
-  geom_segment(aes(x = mean(observed_mean), 
-                   xend = mean(observed_mean), 
-                   y = 0, 
-                   yend = Inf), 
-               color = "#b77fff", 
-               linetype = 'dashed', 
-               linewidth = 1) + 
-  #ggtitle("Distribution of Permuted Mean Relatedness") +
-  labs(x = "Mean kinship", y = 'Frequency') +
+# Combine files
+eligible_permutations <- rbind(agta_permuted, bayaka_permuted, raute_permuted)
+
+png("/Users/inezd/PHD/Chapter_3/Plots/pops_compare_eligible.png", height = 1000, width = 1000)
+ggplot(eligible_permutations, aes(x = population, y = kinship, group = population)) +
+  geom_boxplot(position = position_dodge(width = 0.9), alpha = 0.6, aes(fill = population)) + 
+  scale_fill_manual(values = c("Agta" = "#ffa635", 
+                               "BaYaka" = "#839ffe",
+                               "Raute" = "#ff839f")) +  # Customize dataset colors
+  geom_segment(data = observed_means, aes(x = as.numeric(population) - 0.45, 
+                                          xend = as.numeric(population) + 0.45, 
+                                          y = observed_mean2, 
+                                          yend = observed_mean2,
+                                          group = population,
+                                          color = population),  # Add 'population' to group aesthetics
+               inherit.aes = FALSE,  # Prevent inheriting 'dataset' aesthetic from main plot
+               linetype = "dashed", size = 1) +
+  scale_color_manual(values = c("Agta" = "#ffa635", 
+                                "BaYaka" = "#839ffe",
+                                "Raute" = "#ff839f")) +
   theme_classic() +
-  theme(text = element_text(size = 30))
+  theme(text = element_text(size = 40),
+        legend.position = 'none') +
+  labs(x = "Population", y = "Kinship", fill = "Population") +
+  scale_y_continuous(limits = c(0,0.16), breaks = seq(0,0.16, by = 0.05)) 
 dev.off()
-
-## End of Script
-
